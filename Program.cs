b9825983833f5c48c90da7ca83593cf3854eb13f
John Cox <thecoxcableguy@gmail.com>
2017-06-09 08:26:42 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
